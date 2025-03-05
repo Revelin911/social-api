8fb0b52ecbd3e034ca917ec87e29649ba3a28c52
Revelin911 <revelin911@mail.com>
2025-03-04 21:00:34 -0700

    added node modules and started interface for User

---
+++ src/models/User.ts
@@ -0,0 +1,42 @@
+import { Schema, model, type Document } from 'mongoose';
+
+interface IUser extends Document {
+    username: string,
+    email: string,
+    thoughts: //array of _id values referencing the Thought model
+    friends: //Array of _id values referencing the User model (self-reference)
+}
+//Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+
+// edit all code under
+const courseSchema = new Schema<ICourse>(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        inPerson: {
+            type: Boolean,
+            default: true,
+        },
+        start: {
+            type: Date,
+            default: Date.now(),
+        },
+        end: {
+            type: Date,
+            // Sets a default value of 12 weeks from now
+            default: () => new Date(+new Date() + 84 * 24 * 60 * 60 * 1000),
+        },
+        students: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'student',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        timesta
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,4 @@
-import Student from './Student.js';
+import Student from './Thought.js';
 import Course from './Course.js';
 
 export { Student, Course };
