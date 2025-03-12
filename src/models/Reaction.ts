d4415f3dd575e3e64dbfd65bab8d67f5da28802c
Revelin911 <revelin911@mail.com>
2025-03-12 17:32:14 -0600

    added thought model

--- src/models/Reaction.ts
+++ src/models/Reaction.ts
@@ -8,3 +8,4 @@ interface IReaction extends Document {
     username: string,
     createdAt: Date,
 }
+//This will not be a model, but rather will be used as the reaction field's subdocument schema in the Thought model.
--- src/models/Thought.ts
+++ src/models/Thought.ts
@@ -1,73 +1,73 @@
 import { Schema, Types, model, type Document } from 'mongoose';
 
+interface IReaction {
+    reactionId: Types.ObjectId,
+        thoughtText: string,
+        createdAt: Date,
+        username: string,
+        }
+
 interface IThought extends Document {
     thoughtText: string,
     createdAt: Date,
     username: string,
-    reactions: , //Array of nested documents created with the reactionSchema
+    reactions: IReaction[], //Array of nested documents created with the reactionSchema
 }
 
 //Schema Settings: Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
 
-// edit code under
-interface IStudent extends Document {
-    first: string,
-    last: string,
-    github: string,
-    assignments: Schema.Types.ObjectId[]
-}
-
-const assignmentSchema = new Schema<IAssignment>(
+const reactionSchema = new Schema<IReaction>(
     {
-        assignmentId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId(),
+        reactionId: {
+type: Schema.Types.ObjectId,
+default: () => new Types.ObjectId(),
         },
-        name: {
+        thoughtText: {
             type: String,
             required: true,
-            maxlength: 50,
-            minlength: 4,
-            default: 'Unnamed assignment',
+            maxlength: 280,
         },
-        score: {
-            type: Number,
-            required: true,
-            default: () => Math.floor(Math.random() * (100 - 70 + 1) + 70),
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => new Date(timestamp).toLocaleString(),
+        },
+        username: {
+type: String,
+required: true,
         },
     },
-    {
-        timestamps: true,
-        _id: false
-    }
 );
 
-const studentSchema = new Schema<IStudent>({
-    first: {
+const thoughtSchema = new Schema<IThought>({
+    thoughtText: {
         type: String,
         required: true,
-        max_length: 50,
+        maxlength: 280,
+        minlength: 1,
     },
-    last: {
-        type: String,
-        required: true,
-        max_length: 50,
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => new Date(timestamp).toLocaleString(),
     },
-    github: {
-        type: String,
-        required: true,
-        max_length: 50,
+    username: {
+type: String,
+required: true,
     },
-    assignments: [assignmentSchema],
-},
-    {
+    reactions: [reactionSchema],
+}, {
         toJSON: {
-            getters: true,
-        },
-        timestamps: true
-    }
-);
+        virtuals: true,
+        getters: true,
+        },  
+    });
 
-const Student = model('Student', studentSchema);
+    //Virtual for reaction count
+ thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length
+ });
 
-export default Student;
+const Thought = model<IThought>('Thought', thoughtSchema);
+
+export default Thought;
